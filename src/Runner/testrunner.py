8db1c7773bef5e82b33af6aa8d669b2cdd6bbb7f
deepakkumar130292@gmail.com <deepakkumar130292@gmail.com>
2016-07-02 19:06:07 +0530

    Added __init__.py for importing the module

---
+++ src/Runner/testrunner.py
@@ -0,0 +1,3 @@
+     #suite1 = module1.TheTestSuite()
+       #suite2 = module2.TheTestSuite()
+       #alltests = unittest.TestSuite((suite1, suite2))
---
+++ src/TestSuite/TestSuite.py
@@ -0,0 +1,14 @@
+'''
+Created on Apr 24, 2016
+
+@author: Deepak_Gupta
+'''
+import unittest
+import sys
+from TestCases.Test1 import testcases
+from TestCases.Test2 import testcases1
+class TestSuite1(unittest.TestSuite):
+           def __init__(self):
+               unittest.TestSuite.__init__(self,map(testcases,
+                                                     ("test_case",
+                                                      "test_case1")))
